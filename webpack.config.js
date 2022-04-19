a728a1ad02d91e4d84e49c9b8f7851d396254e0c
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-04-19 16:32:42 +0200

    simplify css loader options

--- webpack.config.js
+++ webpack.config.js
@@ -38,10 +38,7 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: {
-                localIdentName: '[name]__[local]___[hash:base64:5]'
-              },
-              sourceMap: true
+              modules: true
             }
           }
         ]
