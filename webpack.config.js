f32b2aa1625adf32febc2deeb9a3bb949c8fd3b3
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-04-19 15:13:11 +0200

    introduce webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/public/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+};
