712b5971b3412addfb2cff7c3af1beebc900cb03
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-04-20 13:41:45 +0200

    add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  displayName: 'JEST',
+  verbose: true,
+  collectCoverage: true,
+  testTimeout: 10000,
+  setupFilesAfterEnv: ['./tests/setup.js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/tests/', '/src/common/', '/src/helpers/']
+};
