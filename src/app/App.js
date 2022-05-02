2e198c4f20b44b4702e9bc2b183b81e60003bd45
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-05-02 18:17:34 +0200

    add first mantine component

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import loadable from '@loadable/component';
+import { AppShell, Header, Text } from '@mantine/core';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import routes from '~routes';
@@ -6,17 +7,25 @@ import routes from '~routes';
 const DynamicComponent = loadable(props => import(`~views/${props.view}`));
 
 const App = () => (
-  <Routes>
-    {routes.map(({ path, exact, view }) => (
-      <Route
-        path={path}
-        exact={exact}
-        key={`View.${view}`}
-        element={<DynamicComponent view={view} />}
-      />
-    ))}
-    <Route path="*" element={<>404</>} />
-  </Routes>
+  <AppShell
+    header={
+      <Header p={30}>
+        <Text>{"It's a WRAP"}</Text>
+      </Header>
+    }
+  >
+    <Routes>
+      {routes.map(({ path, exact, view }) => (
+        <Route
+          path={path}
+          exact={exact}
+          key={`View.${view}`}
+          element={<DynamicComponent view={view} />}
+        />
+      ))}
+      <Route path="*" element={<>404</>} />
+    </Routes>
+  </AppShell>
 );
 
 export default App;
