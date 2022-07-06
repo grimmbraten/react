dbb84553c958f6eddda7f8c23b005efa8e220388
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-07-06 19:51:04 +0200

    feat: add apollo client

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,20 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { client } from '~apollo/client';
 import AppContextProvider from '~contexts';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
