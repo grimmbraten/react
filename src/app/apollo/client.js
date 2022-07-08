1a60839280b7d1ece4a8e97b5a4a3b0f0662c933
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-07-08 18:56:10 +0200

    feat: introduce apollo client basics

--- src/app/apollo/client.js
+++ src/app/apollo/client.js
@@ -1,6 +1,27 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { getRuntimeConfig } from '~config';
+
+const generateApolloLink = () => {
+  const config = getRuntimeConfig();
+
+  if (!config?.endpoints) {
+    console.warn('no endpoints found in env file');
+    return undefined;
+  }
+
+  return split(
+    operation => operation.getContext().dato === true,
+    new HttpLink({
+      uri: 'endpointUri',
+      headers: {
+        Authorization: `Bearer ${config.endpoints[0].token}`
+      },
+      fetch: (uri, options) => fetch(uri.replace(/endpointUri/gi, config.endpoints[0].uri), options)
+    })
+  );
+};
 
 export const client = new ApolloClient({
-  uri: 'https://flyby-gateway.herokuapp.com/',
+  link: generateApolloLink(),
   cache: new InMemoryCache()
 });
--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,15 @@
 export const getRuntimeConfig = () => ({
   greet: process.env.GREET
+  /*
+  endpoints: [
+    example: {
+      uri: 'endpoint.com',
+      context: 'endpoint',
+      headers: {
+        key: 'name',
+        bearer: process.env.ENDPOINT_BEARER_TOKEN
+      },
+    }
+  ]
+  */
 });
