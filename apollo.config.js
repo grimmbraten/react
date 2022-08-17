b42a671a69af4136453dce44ab87b9a7957a036e
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-08-17 16:15:03 +0200

    feat: introduce ide apollo config for schema fetching

---
+++ apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client: {
+    service: 'my-graphql-api',
+    url: undefined //'https://my-graphql-api.io/simple/v1/endpoint'
+  }
+};
--- src/app/apollo/client.js
+++ src/app/apollo/client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const generateApolloLink = () => undefined;
+const generateApolloLink = () => undefined; //'https://my-graphql-api.io/simple/v1/endpoint'
 
 export const client = new ApolloClient({
   link: generateApolloLink(),
